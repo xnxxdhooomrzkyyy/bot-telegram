69add3bc260c8991afcfc051444b5057c4b771c4
Elliot Alderson <elliotaldersonfsociety2@gmail.com>
2025-09-09 17:30:00 +0700

    Update app.py

--- app.py
+++ app.py
@@ -67,7 +67,7 @@ def base_template(content):
     <head>
       <meta charset="utf-8">
       <meta name="viewport" content="width=device-width, initial-scale=1">
-      <title>Rekap Pengiriman</title>
+      <title>Rekap TTD Toko</title>
       <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
       <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600&display=swap" rel="stylesheet">
       <link rel="manifest" href="{{{{ url_for('static', filename='manifest.json') }}}}">
@@ -147,7 +147,7 @@ def login():
             return redirect(url_for("index"))
         flash("❌ Login gagal!","danger")
     return base_template("""
-    <h3>Login</h3>
+    <h3>Login ke Data Rekapan Toko</h3>
     <form method="POST" class="card p-4">
       <input name="kode_toko" placeholder="Kode Toko" class="form-control mb-2" required>
       <input type="password" name="password" placeholder="Password" class="form-control mb-2" required>
@@ -280,3 +280,4 @@ def export_pdf():
 
 if __name__=="__main__": 
     app.run(host="0.0.0.0", port=5000, debug=True)
+
